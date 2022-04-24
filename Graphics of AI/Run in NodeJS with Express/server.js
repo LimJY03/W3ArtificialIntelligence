a64e805665fc56088f9b9d83ed0d507d75c8057a
DesigneR- <limjunyi03@gmail.com>
2022-04-24 20:46:57 +0800

    Added static directory to "./Example 2"

---
+++ Graphics of AI/Run in NodeJS with Express/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/Example 2 - Graph Plot.html")
+});
+
+app.use(express.static("Example 2"));
+
+server.listen(3000, () => {
+    console.log("listening on *: 3000")
+});
